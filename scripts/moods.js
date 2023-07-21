8791968283e4e8e09567f2308bd5e5e91df63c0c
Tricia Swift <tricia.swift8@gmail.com>
2023-07-21 12:47:36 -0500

    added container for quote and added css styling

--- scripts/moods.js
+++ scripts/moods.js
@@ -7,12 +7,13 @@ export const moodsOutput = () => {
 
   for (const mood of moods) {
     HTMLstring += `<section class="mood_card">
+      <div class="mood_name">${mood.name}</div>  
       <div><img class="mood_img" src="${mood.imageUrl}"/></div>
-      <div class="mood_name">${mood.name}</div>
       <section class="textBox">
-      <div class="mood_text">${mood.text}</div>
-        <div class="mood_quote1">${mood.quotes[0]}</div>
-        <div class="mood_quote2">${mood.quotes[1]}</div>
+        <div class="mood_text">${mood.text}</div>
+        <section class="quotes">
+          <div class="mood_quote1">${mood.quotes[0]}</div>
+          <div class="mood_quote2">${mood.quotes[1]}</div></section>
       </section>
      </section>`;
   }
